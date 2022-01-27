fba171dcf50d0f5190677efcc0c1e14b77fab976
Michael Große <mic.grosse@googlemail.com>
2022-01-27 10:13:13 +0100

    Fix AliasesInput prop and event handling

--- src/components/AliasesInput.vue
+++ src/components/AliasesInput.vue
@@ -15,12 +15,18 @@ const props = defineProps<{
 const emit = defineEmits(['update:modelValue']);
 
 const onInput = (event: string): void => {
-  const aliases = event
+  let data;
+  if (typeof event === 'object') {
+    data = (event as InputEvent).data;
+  } else {
+    data = event;
+  }
+  const aliases = (data as string)
     .split('|')
     .map((alias) => alias.trim())
     .filter((alias) => alias);
   emit('update:modelValue', aliases);
 };
 
-const aliasString = props.modelValue.join(' | ');
+const aliasString = props.modelValue ? props.modelValue.join(' | ') : '';
 </script>
