192bbc804e73355e2dc9fc9613fb5e3075206c69
Michael Große <mic.grosse@googlemail.com>
2022-01-27 10:14:49 +0100

    Build as library and externalize vue
    The dev mode is still broken, but this now works on-wiki.
    
    Cause of dev mode broken seems to be related to vitejs/vite#544. Maybe
    the solution is in there somewhere as well.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
+    target: 'esnext',
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.ts'),
+      name: 'createItemWidget',
+      fileName: (format) => `createItemWidget.${format}.js`,
+    },
     rollupOptions: {
       output: {
-        manualChunks: undefined,
+        globals: {
+          vue: 'Vue',
+        },
       },
+      external: ['vue'],
     },
   },
-})
+});
